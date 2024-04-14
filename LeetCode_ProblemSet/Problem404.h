d6f5dd24147a1246471a9b6aa6fe8106ad8e0c0c
nazmul-islam00 <155834778+nazmul-islam00@users.noreply.github.com>
2024-04-15 03:18:53 +0600

    new solutions

---
+++ LeetCode_ProblemSet/Problem404.h
@@ -0,0 +1,33 @@
+//
+// Created by HP on 4/15/2024.
+//
+
+#ifndef PROBLEM404_H
+#define PROBLEM404_H
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if(root == nullptr)
+            return 0;
+        if(root->left && !root->left->left && !root->left->right)
+            return root->left->val + sumOfLeftLeaves(root->right);
+        return 0 + sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
+    }
+};
+
+
+#endif //PROBLEM404_H
