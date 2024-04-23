f9a50f437a6144612e512014a3dad4e40eea917b
nazmul-islam00 <155834778+nazmul-islam00@users.noreply.github.com>
2024-04-23 23:40:18 +0600

    added new soln

---
+++ LeetCode_ProblemSet/Problem92.h
@@ -0,0 +1,49 @@
+#include <vector>
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        std::vector<int> vals;
+        while(head != nullptr) {
+            vals.push_back(head->val);
+            head = head->next;
+        }
+        int index = 0;
+        if(right - left <= 1) {
+            int temp = vals[left - 1];
+            vals[left - 1] = vals[right - 1];
+            vals[right - 1] = temp;
+        } else {
+            while(left < right) {
+                int temp = vals[left - 1];
+                vals[left++ - 1] = vals[right - 1];
+                vals[right-- - 1] = temp;
+            }
+        }
+        ListNode* answer = new ListNode(vals[0]);
+        ListNode* temp = new ListNode(-1, answer);
+        for(int i = 1; i < vals.size(); i++) {
+            answer = answer->next = new ListNode(vals[i]);
+        }
+        return temp->next;
+    }
+};
