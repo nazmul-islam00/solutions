53094a4f255af34fac929af94239bbb5d552ede6
nazmul-islam00 <155834778+nazmul-islam00@users.noreply.github.com>
2024-05-24 11:27:27 +0600

    updated 2597

--- LeetCode_ProblemSet/Problem2597.h
+++ LeetCode_ProblemSet/Problem2597.h
@@ -10,38 +10,34 @@
 
 
 class Solution {
+private:
+    // currIndex: index of the element being considered
+    // mask: as the subset contains at most 20 bits, it's represented as a mask
+    int countBeautifulSubsets(std::vector<int>& nums, int difference, int currIndex, int mask) {
+        // reached the end of the vector
+        // if any element was included to the subset it has value greater than 0, so count 1
+        // else it is am empty subset, count 0
+        if(currIndex == nums.size()) {
+            return mask > 0 ? 1 : 0;
+        }
+        bool isBeautiful = true;
+        // as the elements are considered sequentially, only checking till the currIndex is enough
+        // the loop can terminate if any corresponding ugly element os found
+        for(int j = 0; j < currIndex && isBeautiful; j++) {
+            // push 1 to the corresponding index and check if it is already in the mask
+            // if already in the mask check for difference
+            isBeautiful = ((1 << j) & mask) == 0 || abs(nums[j] - nums[currIndex]) != difference;
+        }
+        // count subsets not including the currIndex element
+        int skip = countBeautifulSubsets(nums, difference, currIndex + 1, mask);
+        // if isBeautiful, count subsets from next Index including the currIndex
+        // else 0
+        int take = isBeautiful ? countBeautifulSubsets(nums, difference, currIndex + 1, mask + (1 << currIndex)) : 0;
+        return skip + take;
+    }
 public:
     int beautifulSubsets(std::vector<int>& nums, int k) {
-        std::vector<std::vector<short>> ans(std::pow(2, nums.size()), std::vector<short> ());
-        for(int i = 0; i < nums.size(); i++) {
-            int idx = 0;
-            while(idx < ans.size()) {
-                for(int j = 0; j < std::pow(2, nums.size() - 1 - i); j++) {
-                    ans[idx++].push_back(nums[i]);
-                }
-                for(int j = 0; j < std::pow(2, nums.size() - 1 - i); j++) {
-                    idx++;
-                }
-            }
-        }
-        ans.pop_back();
-        int cnt = 0;
-        for(int i = 0; i < ans.size(); i++) {
-            std::unordered_map<short, short> map;
-            bool isBeautiful = true;
-            for(int j = 0; j < ans[i].size(); j++) {
-                if(map.find(ans[i][j] - k) == map.end() && map.find(k + ans[i][j]) == map.end()) {
-                    map[ans[i][j]] = ans[i][j];
-                } else {
-                    isBeautiful = false;
-                     break;
-                }
-            }
-            if(isBeautiful) {
-                cnt++;
-            }
-        }
-        return cnt;
+        return countBeautifulSubsets(nums, k, 0, 0);
     }
 };
 
