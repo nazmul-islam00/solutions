ed38490e39ee3ccba34f612d64c3b306df6d999e
nazmul-islam00 <155834778+nazmul-islam00@users.noreply.github.com>
2024-07-04 12:55:23 +0600

    added new soln

---
+++ LeetCode_ProblemSet/Problem2181.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution(object):
+    def mergeNodes(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        start, end = head, head
+        temp = None
+        sum = 0
+        
+        while end is not None:
+            sum += end.val
+
+            if end.val == 0:
+                start.val = sum
+                temp = start
+                start = start.next
+                sum = 0
+
+            end = end.next
+            
+        temp.next = None
+        head = head.next
+        
+        return head
+        
+        
+        
