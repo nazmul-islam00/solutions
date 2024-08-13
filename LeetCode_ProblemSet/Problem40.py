c403453fb5d436db653577a9823c2d53e7443799
nazmul-islam00 <155834778+nazmul-islam00@users.noreply.github.com>
2024-08-13 11:49:41 +0600

    .

---
+++ LeetCode_ProblemSet/Problem40.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        answer = []
+        self.backtrack(sorted(candidates), target, 0, [], answer)
+        return answer
+    
+    def backtrack(self, candidates, target, idx, path, answer):
+        if target < 0:
+            return
+        if target == 0:
+            answer.append(path)
+            return 
+        for i in range(idx, len(candidates)):
+            if i > idx and candidates[i] == candidates[i - 1]:
+                continue
+            self.backtrack(candidates, target - candidates[i], i + 1, path + [candidates[i]], answer)
