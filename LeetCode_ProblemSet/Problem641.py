dfea64b95c296dc341e5a8bc9b09ac4385f8780b
Nazmul <155834778+nazmul-islam00@users.noreply.github.com>
2024-09-28 23:49:21 +0600

    added new soln

---
+++ LeetCode_ProblemSet/Problem641.py
@@ -0,0 +1,62 @@
+class MyCircularDeque:
+
+    def __init__(self, k: int):
+        self.front = 0
+        self.rear = k - 1
+        self.size = 0
+        self.capacity = k
+        self.queue = [0] * self.capacity
+        
+    def insertFront(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        self.front = (self.front - 1 + self.capacity) % self.capacity
+        self.queue[self.front] = value
+        self.size += 1
+        return True
+
+    def insertLast(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        self.rear = (self.rear + 1) % self.capacity
+        self.queue[self.rear] = value
+        self.size += 1
+        return True
+
+    def deleteFront(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.front = (self.front + 1) % self.capacity
+        self.size -= 1
+        return True
+
+    def deleteLast(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.rear = (self.rear - 1 + self.capacity) % self.capacity
+        self.size -= 1
+        return True
+
+    def getFront(self) -> int:
+        return self.queue[self.front] if not self.isEmpty() else -1
+
+    def getRear(self) -> int:
+        return self.queue[self.rear] if not self.isEmpty() else -1
+ 
+    def isEmpty(self) -> bool:
+        return self.size == 0
+
+    def isFull(self) -> bool:
+        return self.size == self.capacity
+
+
+# Your MyCircularDeque object will be instantiated and called as such:
+# obj = MyCircularDeque(k)
+# param_1 = obj.insertFront(value)
+# param_2 = obj.insertLast(value)
+# param_3 = obj.deleteFront()
+# param_4 = obj.deleteLast()
+# param_5 = obj.getFront()
+# param_6 = obj.getRear()
+# param_7 = obj.isEmpty()
+# param_8 = obj.isFull()
