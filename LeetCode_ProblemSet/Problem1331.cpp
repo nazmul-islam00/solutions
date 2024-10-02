bc90ada30b6026d41dc552acb654dc61d2f92369
Nazmul <155834778+nazmul-islam00@users.noreply.github.com>
2024-10-02 10:24:37 +0600

    added new soln

---
+++ LeetCode_ProblemSet/Problem1331.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <queue>
+#include <functional>
+
+class Solution {
+public:
+    std::vector<int> arrayRankTransform(std::vector<int>& arr) {
+        int cur = INT32_MIN;
+        int rank = 0;
+        std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> heap;
+        for (int i = 0; i < arr.size(); i++) {
+            heap.push({arr[i], i});
+        }
+        
+        while (!heap.empty()) {
+            std::pair<int, int> p = heap.top();
+            heap.pop();
+
+            if (p.first == cur) {
+                arr[p.second] = rank;
+            } else {
+                arr[p.second] = ++rank;
+                cur = p.first;
+            }
+        }
+        return arr;
+    }
+};
