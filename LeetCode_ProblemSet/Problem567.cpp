26f26879a06edf57b781455f50984ed41cc4d743
Nazmul <155834778+nazmul-islam00@users.noreply.github.com>
2024-10-05 07:43:30 +0600

    updated

--- LeetCode_ProblemSet/Problem567.cpp
+++ LeetCode_ProblemSet/Problem567.cpp
@@ -6,38 +6,26 @@ public:
         if (s1.length() > s2.length()) {
             return false;
         }
-        
-        int* target = new int[26];
+
+        int target[26];
         std::memset(target, 0, 26 * sizeof(int));
         for (int i = 0; i < s1.length(); i++) {
             target[s1[i] - 'a']++;
         }
 
-        int* freq = new int[26];
+        int freq[26];
         std::memset(freq, 0, 26 * sizeof(int));
         for (int i = 0; i < s1.length(); i++) {
             freq[s2[i] - 'a']++;
         }
-        bool flag = true;
-        for (int i = 0; i < 26; i++) {
-            if (target[i] != freq[i]) {
-                flag = false;
-            }
-        }
-        if (flag) {
+        if (std::memcmp(target, freq, 26 * sizeof(int)) == 0) {
             return true;
         }
 
         for (int i = 0; i < s2.length() - s1.length(); i++) {
             freq[s2[i] - 'a']--;
             freq[s2[s1.length() + i] - 'a']++;
-            bool flag = true;
-            for (int i = 0; i < 26; i++) {
-                if (target[i] != freq[i]) {
-                    flag = false;
-                }
-            }
-            if (flag) {
+            if (std::memcmp(target, freq, 26 * sizeof(int)) == 0) {
                 return true;
             }
         }
