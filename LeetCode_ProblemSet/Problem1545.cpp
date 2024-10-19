2d1c3fdcc078b16ac394be97bb61fdbdc7efa2f8
Nazmul <155834778+nazmul-islam00@users.noreply.github.com>
2024-10-19 06:52:47 +0600

    added new soln

---
+++ LeetCode_ProblemSet/Problem1545.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <algorithm>
+
+class Solution {
+public:
+    char findKthBit(int n, int k) {
+        if (n == 1)
+            return '0';
+        
+        int len = 1 << n;
+
+        if (k < len / 2)
+            return findKthBit(n - 1, k);
+        
+        if (k == len / 2)
+            return '1';
+        
+        return findKthBit(n - 1, len - k) == '0' ? '1' : '0';
+    }
+};
