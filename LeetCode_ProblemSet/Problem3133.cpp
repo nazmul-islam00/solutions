415ede3adccb906a2e3db7cf43da962d4764983f
Nazmul <155834778+nazmul-islam00@users.noreply.github.com>
2024-11-09 23:46:19 +0600

    added new soln

---
+++ LeetCode_ProblemSet/Problem3133.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long minEnd(int n, int x) {
+        long long result = x;
+        n--;
+        for (long long mask = 1; n > 0; mask <<= 1) {
+            if ((mask & x) == 0) {
+                result |= (n & 1) * mask; // get lsb of n
+                n >>= 1;
+            }
+        }
+        return result;
+    }
+};
