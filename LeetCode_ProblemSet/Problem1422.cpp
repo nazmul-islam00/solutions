99663daee13fc10c287d90f26b1d259b6d99e1e7
Nazmul <155834778+nazmul-islam00@users.noreply.github.com>
2025-01-01 09:24:20 +0600

    updated

--- LeetCode_ProblemSet/Problem1422.cpp
+++ LeetCode_ProblemSet/Problem1422.cpp
@@ -4,19 +4,15 @@
 class Solution {
 public:
     int maxScore(std::string s) {
-        int n = s.length(), zeros = 0, ones;
+        int zeros = 0, ones = 0, score = INT_MIN, n = s.length();
 
-        for (char c : s) {
-            zeros += (c == '0') ? 1 : 0;
+        for (int i = 0; i < n - 1; i++) {
+            zeros += (s[i] == '0') ? 1 : 0;
+            ones += (s[i] == '1') ? 1 : 0;
+            score = std::max(score, zeros - ones);
         }
-        ones = n - zeros;
 
-        int cnt = (s[0] == '0') ? 1 : 0;
-        int score = cnt ? (1 + ones) : (ones - 1);
-        for (int i = 1; i < n - 1; i ++) {
-            cnt += (s[i] == '0') ? 1 : 0;
-            score = std::max(score, cnt + ones - ((i + 1) - cnt));
-        }
-        return score;
+        ones += (s[n - 1] == '1') ? 1 : 0;
+        return score + ones;
     }
 };
