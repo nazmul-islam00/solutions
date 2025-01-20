b9dc0119bc5886088bb1985a52784de6ba8ded8a
Nazmul <155834778+nazmul-islam00@users.noreply.github.com>
2025-01-20 09:59:28 +0600

    updated 2661

--- LeetCode_ProblemSet/Problem2661.cpp
+++ LeetCode_ProblemSet/Problem2661.cpp
@@ -6,17 +6,17 @@ class Solution {
 public:
     int firstCompleteIndex(std::vector<int>& arr, std::vector<std::vector<int>>& mat) {
         int m = mat.size(), n = mat[0].size();
-        std::unordered_map<int, std::pair<int, int>> positions;
+        std::vector<std::pair<int, int>> positions = std::vector<std::pair<int, int>>(m * n);
         std::unordered_map<int, int> rowCnt, colCnt;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                positions[mat[i][j]] = {i, j};
+                positions[mat[i][j] - 1] = {i, j};
             }
         }
 
         for (int i = 0; i < m * n; i++) {
-            auto pos = positions[arr[i]];
+            auto pos = positions[arr[i] - 1];
             int row = pos.first, col = pos.second;
 
             if (rowCnt.find(row) == rowCnt.end()) rowCnt[row] = 1;
