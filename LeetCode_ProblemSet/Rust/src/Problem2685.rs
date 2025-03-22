a7689779906bfe46bb7fccff3fabe4e27a09043f
Nazmul <155834778+nazmul-islam00@users.noreply.github.com>
2025-03-22 17:44:41 +0600

    updated

--- LeetCode_ProblemSet/Rust/src/Problem2685.rs
+++ LeetCode_ProblemSet/Rust/src/Problem2685.rs
@@ -5,10 +5,10 @@ struct DisjointSet {
 }
 
 impl DisjointSet {
-    fn init(&mut self, n: i32) {
-        for idx in 0..n as usize {
-            self.parent[idx] = idx as i32;
-        }
+    fn new(n: usize) -> Self {
+        return Self {
+            parent: (0..n as i32).collect(),
+        };
     }
 
     fn findParent(&mut self, idx: i32) -> i32 {
@@ -19,14 +19,18 @@ impl DisjointSet {
         self.parent[idx as usize] = self.findParent(self.parent[idx as usize]);
         return self.parent[idx as usize];
     }
+
+    fn union(&mut self, idx1: i32, idx2: i32) {
+        let parent1 = self.findParent(idx1);
+        let parent2 = self.findParent(idx2);
+
+        self.parent[parent1 as usize] = parent2;
+    }
 }
 
 impl Solution {
     pub fn count_complete_components(n: i32, edges: Vec<Vec<i32>>) -> i32 {
-        let mut ds = DisjointSet {
-            parent: vec![0; n as usize],
-        };
-        ds.init(n);
+        let mut ds = DisjointSet::new(n as usize);
 
         let mut isConnected = vec![vec![false; n as usize]; n as usize];
 
@@ -37,10 +41,7 @@ impl Solution {
             isConnected[u as usize][v as usize] = true;
             isConnected[v as usize][u as usize] = true;
 
-            let parent1 = ds.findParent(u);
-            let parent2 = ds.findParent(v);
-
-            ds.parent[parent1 as usize] = parent2;
+            ds.union(u, v);
         }
 
         let mut components = vec![vec![0; 0]; n as usize];
