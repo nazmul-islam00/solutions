223521b9c763abc59939a7e5ef19fbb90e39122c
Nazmul <155834778+nazmul-islam00@users.noreply.github.com>
2025-03-30 16:29:22 +0600

    updated

--- LeetCode_ProblemSet/Problem763.cpp
+++ LeetCode_ProblemSet/Problem763.cpp
@@ -5,34 +5,20 @@ class Solution {
    public:
     std::vector<int> partitionLabels(std::string s) {
         std::vector<int> answer;
-        int firstOccurrences[26];
         int lastOccurrences[26];
 
-        for (int idx = 0; idx < 26; idx++) {
-            firstOccurrences[idx] = lastOccurrences[idx] = -1;
-        }
-
         for (int idx = 0; idx < s.length(); idx++) {
-            if (firstOccurrences[s[idx] - 'a'] == -1) firstOccurrences[s[idx] - 'a'] = idx;
             lastOccurrences[s[idx] - 'a'] = idx;
         }
 
-        int idx = 0;
-        while (idx < s.length()) {
-            char c = s[idx];
+        int start = 0, end = 0;
+        for (int idx = 0; idx < s.length(); idx++) {
+            end = std::max(end, lastOccurrences[s[idx] - 'a']);
 
-            int maxLast = lastOccurrences[c - 'a'], initMax = lastOccurrences[c - 'a'];
-            do {
-                initMax = maxLast;
-                for (int i = 0; i < 26; i++) {
-                    if (firstOccurrences[i] < maxLast) {
-                        maxLast = std::max(maxLast, lastOccurrences[i]);
-                    }
-                }
-            } while (maxLast != initMax);
-
-            answer.push_back(maxLast - idx + 1);
-            idx = maxLast + 1;
+            if (idx == end) {
+                answer.push_back(end - start+ 1);
+                start = idx + 1;
+            }
         }
 
         return answer;
